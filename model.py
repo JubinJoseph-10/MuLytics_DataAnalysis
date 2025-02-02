89c1019f92f02a1ef3b7ef5b96f95b7b13d306e3
Jubin Joseph <116001518+JubinJoseph-10@users.noreply.github.com>
2025-02-02 13:50:55 +0530

    Update model.py

--- model.py
+++ model.py
@@ -20,6 +20,11 @@ from sklearn.metrics import accuracy_score
 from sklearn.model_selection import cross_val_score
 
 
+#setting page configs
+logo = Image.open("Images/page_favicon.png")
+st.set_page_config(page_icon=logo,layout='wide',page_title='App Ascend')
+
+
 st.set_option('deprecation.showPyplotGlobalUse', False)
 st.set_page_config(layout='wide')
 
